162995799d7e7f258b6868a105996b2412b8a436
Stephen Blinick <sblinick@stesoft.com>
2020-06-25 19:04:16 -0700

    Some quick hack changes that now only print the longest transitions, the top slowest ops, take multiple files, and summarize.

--- ops_analyzer.rb
+++ ops_analyzer.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 # This script is designed to parse the output of dump_historic_ops and then
 # measure the time spent between different phases. Often when analysing this
 # output, firstly it can be difficult to parse visually because there is so
@@ -30,35 +29,69 @@
 # https://github.com/lathiat/ceph-tools
 # License: GPL v3
 
-measure_between = {"reached_pg" => ["journaled_completion_queued"],
-                   "initiated" => ["commit_sent", "journaled_completion_queued", "reached_pg", "op_commit"],
-                   "started" => ["commit_queued_for_journal_write"],
-                   "commit_queued_for_journal_write" => ["journaled_completion_queued", "op_commit"],
+# Modified by Stephen Blinick 
+measure_between = {"reached_pg" => ["started"],
+                   "initiated" => ["queued_for_pg"],
+                   "queued_for_pg" => ["reached_pg"],
+                   "journal_completion_queued" => ["sub_op_comitted"],
+                   "sub_op_comitted" => ["sub_op_applied"],
+                   "sub_op_applied" => ["commit_sent"],
+                   "commit_sent" => ["done"],
 }
+
+# ORIGINAL
+#measure_between = {"reached_pg" => ["journaled_completion_queued"],
+#                   "initiated" => ["commit_sent", "journaled_completion_queued", "reached_pg", "op_commit"],
+#                   "started" => ["commit_queued_for_journal_write"],
+#                   "commit_queued_for_journal_write" => ["journaled_completion_queued", "op_commit"],
+#}
 
 require 'pp'
 require 'json'
 require 'date'
 
-input_fn = ARGV[0]
-input_file = open(input_fn)
-j = JSON.parse(input_file.read)
+#j = JSON.parse('{"network_nothingburgers": "delicious"}')
+j = Array.new
+ARGV.each { |fname|
+   pp fname
+   input_file = open(fname)
+   nf = JSON.parse(input_file.read)
+   #j.merge!(nf)
+   if nf["ops"]
+     j.concat(nf["ops"])
+   else
+     j.concat(nf["Ops"])
+   end
+}
+#input_fn = ARGV[0]
+#input_file = open(input_fn)
+#j = JSON.parse(input_file.read)
+#pp j
 ops = {}
 
-if j["ops"]
-    op_in = j["ops"]
-else
-    op_in = j["Ops"]
-end
+#if j["ops"]
+#    op_in = j["ops"]
+#else
+#    op_in = j["Ops"]
+#end
 
 interesting = []
 
-op_in.each do |op|
+mega_ops = []
+
+#op_in.each do |op|
+j.each do |op|
     key = op["description"]
-    raise if ops[key]
+    # HACK just skip dup op descriptions, only seems to be weird things like scrub NOT SUBOPS
+    #raise if ops[key]
+    next if ops[key]
     ops[key] = {}
     ops[key][:op] = op
-    state, client, steps = op["type_data"]
+    infostuff = op["type_data"]
+    state, client, steps = infostuff[""]
+    state = "sucks"
+    steps = infostuff["events"]
+    client = infostuff["client_info"]
 
     times = {}
     initiated_t = DateTime.parse(op["initiated_at"]).to_time
@@ -99,9 +132,68 @@ op_in.each do |op|
     next if times["waiting for rw locks-reached_pg"]
 
 
-    print "-----\n"
-    pp op
-    pp times.to_a.sort{|b, a| a[1] <=> b[1] }
-    #if times["until journaled_completion_queued"].to_f > 1 and times["until reached_pg"].to_f < 1
-    #end
+    #pp op
+    ttltime = op["duration"]
+    agetime = op["age"]
+    desc = op["description"]
+
+    #print "== OP == %s, age %s took %s\n" % [desc, agetime, ttltime]
+    sorttimes = times.to_a.sort{|b, a| a[1] <=> b[1] }
+    opout = [ agetime, desc, ttltime, sorttimes.take(3) ]
+    mega_ops.append(opout)
+
+    sorttimes.take(3).each do |obj|
+      ptc = obj[1].to_f / ttltime.to_f * 100.0
+      #print "  > %s (%3.2f) = %3.0f %%\n" % [obj[0], obj[1].to_f, ptc]
+    end
+end
+
+printtop=20
+print "------------------------------\n"
+print "----- Top %d longest ops \n" % [printtop]
+print "------------------------------\n"
+mega_ops.to_a.sort{|b, a| a[2] <=> b[2] }.take(printtop).each do |opout|
+    ttltime = opout[2]
+    agetime = opout[0]
+    desc = opout[1]
+    print "== OP == %s, age %s took %s\n" % [desc, agetime, ttltime]
+    opout[3].each do |obj|
+      ptc = obj[1].to_f / ttltime.to_f * 100.0
+      print "  > %s (%3.2f) = %3.0f %%\n" % [obj[0], obj[1].to_f, ptc]
+    end
+end
+
+# ======== one more time but summarize everything
+
+totalops = 0
+megatimes = Hash.new
+megapct = Hash.new
+
+mega_ops.to_a.sort{|b, a| a[2] <=> b[2] }.each do |opout|
+    totalops = totalops+1
+    ttltime = opout[2]
+    agetime = opout[0]
+    desc = opout[1]
+    opout[3].each do |obj|
+      ptc = obj[1].to_f / ttltime.to_f * 100.0
+      if megatimes.include? obj[0]
+         megatimes[obj[0]] += obj[1].to_f
+         megapct[obj[0]] += ptc
+      else
+         megatimes[obj[0]] = obj[1].to_f
+         megapct[obj[0]] = ptc
+      end
+    end
+end
+print "------------------------------\n"
+print "----- Summary of all ops \n" % [printtop]
+print "------------------------------\n"
+print "Total # Ops analyzed: %d \n" % [totalops]
+print "* Across all ops, spots that took most total time:\n"
+megatimes.to_a.sort{|b, a| a[1] <=> b[1] }.each do |megatime|
+  print "    >> %s -- Total %3.2f seconds\n" % [megatime[0], megatime[1]]
+end
+print "* Across all ops, average % of time each spot took in the op:\n"
+megapct.to_a.sort{|b, a| a[1] <=> b[1] }.each do |megap|
+  print "    >> %s -- Avg %3.2f%% \n" % [megap[0], megap[1]/totalops]
 end
